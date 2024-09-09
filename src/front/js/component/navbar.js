1cd094f8815d4254d9b148e65a34e6ce3556c466
Yuvinska Medrano Sandoval <168204816+yuvinska@users.noreply.github.com>
2024-09-09 09:37:01 +0000

    segundo navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,15 +3,20 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav class="navbar navbar-expand-lg navbar-light">
+			<div class="container">
+				<a class="navbar-brand" href="#"><img src="https://res.cloudinary.com/dyvut6idr/image/upload/v1725640842/Logo_PetStar-removebg-preview_oo91wx.png" alt="PetStar" height="40"/></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav mx-auto">
+						<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Foro</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Search</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Services</a></li>
+						<li class="nav-item"><a class="btn btn-primary ml-2" href="#">Log out</a></li>
+					</ul>
 				</div>
 			</div>
 		</nav>
