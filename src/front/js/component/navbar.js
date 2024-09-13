7f22cf03f4769a45f3cc47b65fa86693657447ce
Yuvinska Medrano Sandoval <168204816+yuvinska@users.noreply.github.com>
2024-09-13 18:13:44 +0000

    no nabvar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,22 +3,6 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg navbar-light">
-			<div class="container">
-				<a class="navbar-brand" href="#"><img src="https://res.cloudinary.com/dyvut6idr/image/upload/v1725640842/Logo_PetStar-removebg-preview_oo91wx.png" alt="PetStar" height="40" /></a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-						<li class="nav-item"><a class="nav-link" href="#">Foro</a></li>
-						<li class="nav-item"><a class="btn btn-warning text-white ml-2" href="#">Join PetStar Now!</a></li>
-						<li class="nav-item"><a class="nav-link" href="#">Log In</a></li>
-						<li class="nav-item"><a class="btn btn-primary ml-2" href="#">Sign Up</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		
 	);
 };
