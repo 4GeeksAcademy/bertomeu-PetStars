b2ddb8a08375ed90066c5c201c852db13804d358
Ocaanaaa <166707320+Ocaanaaa@users.noreply.github.com>
2024-09-18 17:26:21 +0000

    [Final Version Forum Login SignUp]

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,14 +2,22 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
+
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import LoginPage from "./pages/login";
+import SignupPage from "./pages/signup";
+import ProfilePage from "./pages/ProfilePage";
+import ForumPage from './pages/forum';
+import Profiles from "./pages/profiles";
 
 //create your first component
 const Layout = () => {
@@ -17,7 +25,7 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
@@ -29,6 +37,12 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
+                        <Route element={<LoginPage />} path="/login" />
+                        <Route element={<SignupPage />} path="/signup" />
+                        <Route element={<ProfilePage />} path="/yourprofile" />
+                        <Route element={<ForumPage />} path="/forum" />
+                        <Route element={<Profiles />} path="/profiles" />
+
                     </Routes>
                     <Footer />
                 </ScrollToTop>
