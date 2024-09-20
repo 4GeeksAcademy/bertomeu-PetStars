dd9c45752cd0413f6c4542754fd1a928f5771aa2
bertomeu <josecarlosbertomeu@gmail.com>
2024-09-20 19:00:14 +0000

    modified layout.js

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -15,19 +15,13 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import LoginPage from "./pages/login";
 import SignupPage from "./pages/signup";
-import ProfilePage from "./pages/ProfilePage";
 import ForumPage from './pages/forum';
-import Profiles from "./pages/profiles";
 import General from "./pages/general";
-
-import LoginPage from "./pages/login";
 import { Cloudinary } from "./pages/cloudinary";
 import RestorePassword from "./pages/restorePassword";
-
-
-
 import Profile from "./pages/profile";
 import { ProfilePage } from "./pages/ProfilePage";
+
 
 
 //create your first component
@@ -52,13 +46,13 @@ const Layout = () => {
 
                         <Route element={<Cloudinary />} path="/cloudinary" />
                         <Route element={<RestorePassword />} path="/restorePassword/:uuid" />
-
+                        <Route element={<ProfilePage />} path="/profilePage" /> 
                         <Route element={<SignupPage />} path="/signup" />
-                        <Route element={<ProfilePage />} path="/yourprofile" />
+                        
                         <Route element={<ForumPage />} path="/forum" />
-                        <Route element={<Profiles />} path="/profiles" />
+                        
                         <Route element={<Profile />} path="/profile" />
-                        <Route element={<ProfilePage />} path="/profilepage" />
+                        
 
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} path="*" />
